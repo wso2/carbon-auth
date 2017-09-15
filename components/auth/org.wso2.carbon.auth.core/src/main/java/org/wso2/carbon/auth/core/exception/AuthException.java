8dfd9312752a08f906ed6cef8ffd3c9f2c6dbfd8
Lakmali <lakmali8781@gmail.com>
2017-09-15 11:31:54 +0530

    Adding exception handling base classes

---
+++ components/auth/org.wso2.carbon.auth.core/src/main/java/org/wso2/carbon/auth/core/exception/AuthException.java
@@ -0,0 +1,97 @@
+/*
+ *
+ *   Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *   WSO2 Inc. licenses this file to you under the Apache License,
+ *   Version 2.0 (the "License"); you may not use this file except
+ *   in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+
+package org.wso2.carbon.auth.core.exception;
+
+/**
+ * Top level exception class of Auth exceptions
+ *
+ */
+public class AuthException extends Exception {
+    
+    private ErrorHandler errorHandler;
+
+    /**
+     * Get error handler object.
+     * @return ErrorHandler
+     */
+    public ErrorHandler getErrorHandler() {
+        return errorHandler;
+    }
+
+    /**
+     * @param message Error message
+     * @param cause Error cause
+     */
+    public AuthException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     *
+     * @param cause Error cause
+     */
+    public AuthException(Throwable cause) {
+        super(cause);
+        this.errorHandler = ExceptionCodes.INTERNAL_ERROR;
+    }
+
+    /**
+     *
+     * @param message Error message
+     * @param cause Error cause
+     * @param enableSuppression whether you need enable suppression
+     * @param writableStackTrace Writable error stack trace.
+     */
+    protected AuthException(String message, Throwable cause, boolean enableSuppression,
+            boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    /**
+     *
+     * @param message Error message
+     */
+    public AuthException(String message) {
+        super(message);
+    }
+
+    /**
+     * This is a default constructure where you can pass error code to error DTO
+     * @param message Error message
+     * @param code Exception code that need to pass to the error DTO
+     */
+    public AuthException(String message, ErrorHandler code) {
+        super(message);
+        this.errorHandler = code;
+    }
+
+    /**
+     * This is a default constructure where you can pass error code to error DTO
+     * @param message Error message
+     * @param cause throwable object.
+     * @param code Exception code that need to pass to the error DTO
+     */
+    public AuthException(String message, Throwable cause, ErrorHandler code) {
+        super(message, cause);
+        this.errorHandler = code;
+    }
+
+}
---
+++ components/auth/org.wso2.carbon.auth.core/src/main/java/org/wso2/carbon/auth/core/exception/ExceptionCodes.java
@@ -0,0 +1,70 @@
+/*
+ *
+ *   Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *   WSO2 Inc. licenses this file to you under the Apache License,
+ *   Version 2.0 (the "License"); you may not use this file except
+ *   in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+
+package org.wso2.carbon.auth.core.exception;
+
+/**
+ * This enum class holds error codes that we need to pass to upper level. 
+ * You have to define your custom error codes here.
+ */
+public enum ExceptionCodes implements ErrorHandler{
+    
+    INTERNAL_ERROR(900967, "General Error", 500, "Server Error Occurred");
+    
+    private final long errorCode;
+    private final String errorMessage;
+    private final int httpStatusCode;
+    private final String errorDescription;
+
+    /**
+     * @param errorCode        This is unique error code that pass to upper level.
+     * @param msg              The error message that you need to pass along with the error code.
+     * @param httpErrorCode    This HTTP status code which should return from REST API layer. If you don't want to pass
+     *                         a http status code keep it blank.
+     * @param errorDescription The error description.
+     */
+    ExceptionCodes(long errorCode, String msg, int httpErrorCode, String errorDescription) {
+        this.errorCode = errorCode;
+        this.errorMessage = msg;
+        this.httpStatusCode = httpErrorCode;
+        this.errorDescription = errorDescription;
+    }
+
+    @Override
+    public long getErrorCode() {
+        return this.errorCode;
+    }
+
+    @Override
+    public String getErrorMessage() {
+        return this.errorMessage;
+    }
+
+    @Override
+    public int getHttpStatusCode() {
+        return this.httpStatusCode;
+    }
+
+    @Override
+    public String getErrorDescription() {
+        return this.errorDescription;
+    }
+
+}
