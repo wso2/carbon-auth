0c4ad96559352e425b80fe833f5694b90dc202a8
mushthaq33 <mushthaq33@gmail.com>
2017-09-20 14:31:38 +0530

    Adding client registration implementation changes

---
+++ components/auth/org.wso2.carbon.auth.client.registration.rest.api/src/main/java/org/wso2/carbon/auth/client/registration/rest/api/utils/DCRMUtils.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * you may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.carbon.auth.client.registration.rest.api.utils;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class DCRMUtils {
+
+    private static final Log log = LogFactory.getLog(DCRMUtils.class);
+
+    public static boolean isRedirectionUriValid(String redirectUri) {
+
+        if (log.isDebugEnabled()) {
+            log.debug("Validating uri: " + redirectUri);
+        }
+
+        if (isBlank(redirectUri)) {
+            log.error("The redirection URI is either null or blank.");
+            return false;
+        }
+
+        try {
+            //Trying to parse the URI, just to verify the URI syntax is correct.
+            new URI(redirectUri);
+        } catch (URISyntaxException e) {
+            String errorMessage = "The redirection URI: " + redirectUri + ", is not a valid URI.";
+            log.error(errorMessage, e);
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isBlank(String input) {
+        if (StringUtils.isBlank(input) || "null".equals(input.trim())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ components/auth/org.wso2.carbon.auth.client.registration/src/main/java/org/wso2/carbon/auth/client/registration/dao/impl/DAOFactory.java
@@ -0,0 +1,64 @@
+/*
+ *
+ *   Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *   WSO2 Inc. licenses this file to you under the Apache License,
+ *   Version 2.0 (the "License"); you may not use this file except
+ *   in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+
+package org.wso2.carbon.auth.client.registration.dao.impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.wso2.carbon.auth.client.registration.ClientRegistrationDAOException;
+import org.wso2.carbon.auth.client.registration.dao.ApplicationDAO;
+import org.wso2.carbon.auth.core.datasource.DAOUtil;
+import org.wso2.carbon.auth.core.exception.ExceptionCodes;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * DAO Object creation factory
+ */
+public class DAOFactory {
+    private static final Logger log = LoggerFactory.getLogger(DAOFactory.class);
+
+    private static final String MYSQL = "MySQL";
+    private static final String H2 = "H2";
+    private static final String DB2 = "DB2";
+    private static final String MICROSOFT = "Microsoft";
+    private static final String MS_SQL = "MS SQL";
+    private static final String POSTGRE = "PostgreSQL";
+    private static final String ORACLE = "Oracle";
+
+    public static ApplicationDAO getApplicationDAO() throws ClientRegistrationDAOException {
+        try (Connection connection = DAOUtil.getAuthConnection()) {
+            String driverName = connection.getMetaData().getDriverName();
+
+            if (!(driverName.contains(MYSQL) || driverName.contains(H2) || driverName.contains(DB2) ||
+                    driverName.contains(MS_SQL) || driverName.contains(MICROSOFT) || driverName.contains(POSTGRE) ||
+                    driverName.contains(ORACLE))) {
+                throw new ClientRegistrationDAOException("Unhandled DB driver: " + driverName + " detected",
+                        ExceptionCodes.DAO_EXCEPTION);
+            }
+
+            return new ApplicationDAOImpl();
+        } catch (SQLException e) {
+            throw new ClientRegistrationDAOException("Error while getting applicationDAO", e);
+        }
+    }
+    
+}
