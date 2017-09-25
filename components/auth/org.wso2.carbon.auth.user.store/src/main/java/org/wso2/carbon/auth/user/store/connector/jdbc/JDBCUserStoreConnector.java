3ee29c77a99f5fca9042a5920e802e3d169c27ee
Lakmali <lakmali8781@gmail.com>
2017-09-25 10:40:07 +0530

    Fixing imports

--- components/auth/org.wso2.carbon.auth.user.store/src/main/java/org/wso2/carbon/auth/user/store/connector/jdbc/JDBCUserStoreConnector.java
+++ components/auth/org.wso2.carbon.auth.user.store/src/main/java/org/wso2/carbon/auth/user/store/connector/jdbc/JDBCUserStoreConnector.java
@@ -42,7 +42,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import javax.security.auth.callback.Callback;
 import javax.sql.DataSource;
