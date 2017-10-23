2969f3d85567d5a4dc0f774d320d2500736be194
Niveathika <niveathika@wso2.com>
2017-10-23 13:22:03 +0530

    Implementation of Groups, Me and Users API

--- components/auth/org.wso2.carbon.auth.scim.rest.api/src/main/java/org/wso2/carbon/auth/scim/rest/api/impl/GroupsApiServiceImpl.java
+++ components/auth/org.wso2.carbon.auth.scim.rest.api/src/main/java/org/wso2/carbon/auth/scim/rest/api/impl/GroupsApiServiceImpl.java
@@ -1,5 +1,9 @@
 package org.wso2.carbon.auth.scim.rest.api.impl;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.auth.core.exception.AuthUserManagementException;
+import org.wso2.carbon.auth.scim.impl.SCIMManager;
 import org.wso2.carbon.auth.scim.rest.api.*;
 import org.wso2.carbon.auth.scim.rest.api.dto.*;
 
@@ -9,6 +13,11 @@ import org.wso2.carbon.auth.scim.rest.api.NotFoundException;
 
 import java.io.InputStream;
 
+import org.wso2.carbon.auth.scim.rest.api.util.ApiServiceUtils;
+import org.wso2.charon3.core.extensions.UserManager;
+import org.wso2.charon3.core.protocol.SCIMResponse;
+import org.wso2.charon3.core.protocol.endpoints.GroupResourceManager;
+import org.wso2.charon3.core.protocol.endpoints.UserResourceManager;
 import org.wso2.msf4j.formparam.FormDataParam;
 import org.wso2.msf4j.formparam.FileInfo;
 import org.wso2.msf4j.Request;
@@ -16,36 +25,79 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 
 public class GroupsApiServiceImpl extends GroupsApiService {
+    private static final Log LOG = LogFactory.getLog(GroupsApiServiceImpl.class);
+
     @Override
     public Response groupsGet(Integer startIndex
 , Integer count
 , String filter
   ,Request request) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        UserManager userManager;
+        try {
+            userManager = SCIMManager.getInstance().getCarbonAuthUserManager();
+            GroupResourceManager groupResourceManager = new GroupResourceManager();
+            SCIMResponse scimResponse = groupResourceManager.listWithGET(userManager,  filter, startIndex, count,
+                    null, null, null, null);
+            return ApiServiceUtils.buildResponse(scimResponse);
+        } catch (AuthUserManagementException e) {
+            LOG.error("Error in initializing the CarbonAuthUserManager");
+        }
+        return Response.serverError().build();
     }
     @Override
     public Response groupsIdDelete(String id
   ,Request request) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        UserManager userManager;
+        try {
+            userManager = SCIMManager.getInstance().getCarbonAuthUserManager();
+            GroupResourceManager groupResourceManager = new GroupResourceManager();
+            SCIMResponse scimResponse = groupResourceManager.delete(id, userManager);
+            return ApiServiceUtils.buildResponse(scimResponse);
+        } catch (AuthUserManagementException e) {
+            LOG.error("Error in initializing the CarbonAuthUserManager");
+        }
+        return Response.serverError().build();
     }
     @Override
     public Response groupsIdGet(String id
   ,Request request) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        UserManager userManager;
+        try {
+            userManager = SCIMManager.getInstance().getCarbonAuthUserManager();
+            GroupResourceManager groupResourceManager = new GroupResourceManager();
+            SCIMResponse scimResponse = groupResourceManager.get(id, userManager,  null, null);
+            return ApiServiceUtils.buildResponse(scimResponse);
+        } catch (AuthUserManagementException e) {
+            LOG.error("Error in initializing the CarbonAuthUserManager");
+        }
+        return Response.serverError().build();
     }
     @Override
     public Response groupsIdPut(String id, GroupDTO body
   ,Request request) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        UserManager userManager;
+        try {
+            userManager = SCIMManager.getInstance().getCarbonAuthUserManager();
+            GroupResourceManager groupResourceManager = new GroupResourceManager();
+            SCIMResponse scimResponse = groupResourceManager.updateWithPUT(id, body.toString(), userManager,  null, null);
+            return ApiServiceUtils.buildResponse(scimResponse);
+        } catch (AuthUserManagementException e) {
+            LOG.error("Error in initializing the CarbonAuthUserManager");
+        }
+        return Response.serverError().build();
     }
     @Override
     public Response groupsPost(GroupDTO body
   ,Request request) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        UserManager userManager;
+        try {
+            userManager = SCIMManager.getInstance().getCarbonAuthUserManager();
+            GroupResourceManager groupResourceManager = new GroupResourceManager();
+            SCIMResponse scimResponse = groupResourceManager.create(body.toString(), userManager,  null, null);
+            return ApiServiceUtils.buildResponse(scimResponse);
+        } catch (AuthUserManagementException e) {
+            LOG.error("Error in initializing the CarbonAuthUserManager");
+        }
+        return Response.serverError().build();
     }
 }
--- components/auth/org.wso2.carbon.auth.scim.rest.api/src/main/java/org/wso2/carbon/auth/scim/rest/api/impl/MeApiServiceImpl.java
+++ components/auth/org.wso2.carbon.auth.scim.rest.api/src/main/java/org/wso2/carbon/auth/scim/rest/api/impl/MeApiServiceImpl.java
@@ -1,5 +1,9 @@
 package org.wso2.carbon.auth.scim.rest.api.impl;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.auth.core.exception.AuthUserManagementException;
+import org.wso2.carbon.auth.scim.impl.SCIMManager;
 import org.wso2.carbon.auth.scim.rest.api.*;
 import org.wso2.carbon.auth.scim.rest.api.dto.*;
 
@@ -9,6 +13,11 @@ import org.wso2.carbon.auth.scim.rest.api.NotFoundException;
 
 import java.io.InputStream;
 
+import org.wso2.carbon.auth.scim.rest.api.util.ApiServiceUtils;
+import org.wso2.charon3.core.extensions.UserManager;
+import org.wso2.charon3.core.protocol.SCIMResponse;
+import org.wso2.charon3.core.protocol.endpoints.GroupResourceManager;
+import org.wso2.charon3.core.protocol.endpoints.MeResourceManager;
 import org.wso2.msf4j.formparam.FormDataParam;
 import org.wso2.msf4j.formparam.FileInfo;
 import org.wso2.msf4j.Request;
@@ -16,24 +25,74 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 
 public class MeApiServiceImpl extends MeApiService {
+    private static final Log LOG = LogFactory.getLog(GroupsApiServiceImpl.class);
+
     @Override
     public Response meDelete( Request request) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        String userUniqueId = getUserId(request);
+        UserManager userManager;
+        try {
+            userManager = SCIMManager.getInstance().getCarbonAuthUserManager();
+            MeResourceManager meResourceManager = new MeResourceManager();
+            SCIMResponse scimResponse = meResourceManager.delete(userUniqueId, userManager);
+            return ApiServiceUtils.buildResponse(scimResponse);
+        } catch (AuthUserManagementException e) {
+            LOG.error("Error in initializing the CarbonAuthUserManager");
+        }
+        return Response.serverError().build();
     }
     @Override
     public Response meGet( Request request) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        String userUniqueId = getUserId(request);
+        UserManager userManager;
+        try {
+            userManager = SCIMManager.getInstance().getCarbonAuthUserManager();
+            MeResourceManager meResourceManager = new MeResourceManager();
+            SCIMResponse scimResponse = meResourceManager.get(userUniqueId, userManager, null, null);
+            return ApiServiceUtils.buildResponse(scimResponse);
+        } catch (AuthUserManagementException e) {
+            LOG.error("Error in initializing the CarbonAuthUserManager");
+        }
+        return Response.serverError().build();
     }
+
     @Override
-    public Response mePost( Request request) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
-    }
+    public Response mePost(UserDTO body, Request request) throws NotFoundException {
+        String userUniqueId = getUserId(request);
+        UserManager userManager;
+        try {
+            userManager = SCIMManager.getInstance().getCarbonAuthUserManager();
+            MeResourceManager meResourceManager = new MeResourceManager();
+            SCIMResponse scimResponse = meResourceManager.create(body.toString(), userManager, null, null);
+            return ApiServiceUtils.buildResponse(scimResponse);
+        } catch (AuthUserManagementException e) {
+            LOG.error("Error in initializing the CarbonAuthUserManager");
+        }
+        return Response.serverError().build();    }
+
     @Override
-    public Response mePut( Request request) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+    public Response mePut(UserDTO body, Request request) throws NotFoundException {
+        String userUniqueId = getUserId(request);
+        UserManager userManager;
+        try {
+            userManager = SCIMManager.getInstance().getCarbonAuthUserManager();
+            MeResourceManager meResourceManager = new MeResourceManager();
+            SCIMResponse scimResponse = meResourceManager.updateWithPUT(userUniqueId, body.toString(), userManager,
+                    null, null);
+            return ApiServiceUtils.buildResponse(scimResponse);
+        } catch (AuthUserManagementException e) {
+            LOG.error("Error in initializing the CarbonAuthUserManager");
+        }
+        return Response.serverError().build();    }
+
+    private String getUserId(Request request) {
+        Object authzUser = request.getProperty("authzUser");
+        String userUniqueId = null;
+        if (authzUser instanceof String) {
+            userUniqueId = (String) authzUser;
+        } else {
+            LOG.error("User id not found in the request.");
+        }
+        return userUniqueId;
     }
 }
