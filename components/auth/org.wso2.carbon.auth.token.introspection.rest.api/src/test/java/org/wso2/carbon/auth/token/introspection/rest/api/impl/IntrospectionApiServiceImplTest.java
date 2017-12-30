ceb6aee94b4db8a2c1ac8bc789730da97baac91d
Rukshan <ruky68@live.com>
2017-12-30 17:52:40 +0530

    added org.wso2.carbon.auth.token.introspection.rest.api test

---
+++ components/auth/org.wso2.carbon.auth.token.introspection.rest.api/src/test/java/org/wso2/carbon/auth/token/introspection/rest/api/impl/IntrospectionApiServiceImplTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.carbon.auth.token.introspection.rest.api.impl;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.wso2.carbon.auth.token.introspection.IntrospectionException;
+import org.wso2.carbon.auth.token.introspection.dto.IntrospectionContext;
+import org.wso2.carbon.auth.token.introspection.dto.IntrospectionResponse;
+import org.wso2.carbon.auth.token.introspection.impl.IntrospectionManagerImpl;
+import org.wso2.carbon.auth.token.introspection.impl.TokenValidatorHandlerImpl;
+import org.wso2.carbon.auth.token.introspection.rest.api.IntrospectionApiService;
+import org.wso2.carbon.auth.token.introspection.rest.api.dto.IntrospectionResponseDTO;
+import org.wso2.carbon.auth.token.introspection.rest.api.factories.IntrospectionApiServiceFactory;
+
+import javax.ws.rs.core.Response;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ IntrospectionManagerImpl.class, TokenValidatorHandlerImpl.class, IntrospectionContext.class,
+         })
+
+public class IntrospectionApiServiceImplTest {
+
+    @Mock
+    TokenValidatorHandlerImpl tokenValidatorHandler;
+    @Mock
+    IntrospectionContext context;
+
+    @Test
+    public void testIntrospect() throws Exception {
+        IntrospectionApiService service = IntrospectionApiServiceFactory.getIntrospectionApi();
+        IntrospectionResponse introspectionResponse = new IntrospectionResponse();
+        introspectionResponse.setActive(true);
+
+        PowerMockito.mockStatic(TokenValidatorHandlerImpl.class);
+        PowerMockito.mockStatic(IntrospectionContext.class);
+
+        PowerMockito.whenNew(TokenValidatorHandlerImpl.class).withNoArguments().thenReturn(tokenValidatorHandler);
+        PowerMockito.whenNew(IntrospectionContext.class).withNoArguments().thenReturn(context);
+        PowerMockito.when(context.getIntrospectionResponse()).thenReturn(introspectionResponse);
+
+        Response response = service.introspect("ASDF5F6sd587fdgfsakjdd", null);
+        IntrospectionResponseDTO dto = (IntrospectionResponseDTO) response.getEntity();
+        Assert.assertTrue(dto.getActive());
+
+        PowerMockito.when(tokenValidatorHandler, "validate", Mockito.any(IntrospectionContext.class))
+                .thenThrow(IntrospectionException.class);
+        response = service.introspect("inactive", null);
+        dto = (IntrospectionResponseDTO) response.getEntity();
+        Assert.assertFalse(dto.getActive());
+    }
+}
