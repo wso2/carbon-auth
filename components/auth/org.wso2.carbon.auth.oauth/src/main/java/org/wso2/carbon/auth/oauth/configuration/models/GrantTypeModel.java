6c10e75e872aa993857b050e213a31f5b98aee5d
Rukshan <ruky68@live.com>
2018-04-09 17:18:30 +0530

    Added missing classes

---
+++ components/auth/org.wso2.carbon.auth.oauth/src/main/java/org/wso2/carbon/auth/oauth/configuration/models/GrantTypeModel.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.carbon.auth.oauth.configuration.models;
+
+import org.wso2.carbon.config.annotation.Element;
+
+/**
+ *
+ */
+public class GrantTypeModel {
+    @Element(description = "Access token default validity period")
+    private String name = "password";
+    @Element(description = "Default grant types")
+    private String className = "classname";
+
+    public GrantTypeModel(String name, String className) {
+        this.name = name;
+        this.className = className;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+}
