dccf917d7f708da613251457c6c442365b7b17b6
Pubudu Gunatilaka <pubudu538@gmail.com>
2018-04-10 14:40:12 +0530

    Adding UserInfoFactory to get different response builders

--- components/auth/org.wso2.carbon.auth.oauth.rest.api/src/main/java/org/wso2/carbon/auth/oauth/rest/api/factories/UserinfoApiServiceFactory.java
+++ components/auth/org.wso2.carbon.auth.oauth.rest.api/src/main/java/org/wso2/carbon/auth/oauth/rest/api/factories/UserinfoApiServiceFactory.java
@@ -4,19 +4,26 @@ import org.wso2.carbon.auth.oauth.rest.api.UserinfoApiService;
 import org.wso2.carbon.auth.oauth.rest.api.impl.UserinfoApiServiceImpl;
 import org.wso2.carbon.auth.token.introspection.IntrospectionManager;
 import org.wso2.carbon.auth.token.introspection.impl.IntrospectionManagerImpl;
-import org.wso2.carbon.auth.user.info.UserInfoJSONResponseBuilder;
+import org.wso2.carbon.auth.user.info.UserInfoResponseBuilder;
+import org.wso2.carbon.auth.user.info.exception.UserInfoException;
+import org.wso2.carbon.auth.user.info.impl.UserInfoFactory;
 import org.wso2.carbon.auth.user.info.impl.UserInfoRequestHandlerImpl;
 
 /**
  * Factory class to be used for Userinfo API
- *
  */
 public class UserinfoApiServiceFactory {
 
     public static UserinfoApiService getUserinfoApi() {
+
         IntrospectionManager introspectionManager = new IntrospectionManagerImpl();
-        return new UserinfoApiServiceImpl(new UserInfoRequestHandlerImpl(introspectionManager,
-                new UserInfoJSONResponseBuilder()));
+        try {
+            UserInfoResponseBuilder userInfoResponseBuilder = UserInfoFactory.getUserInfoResponseBuilder();
+            return new UserinfoApiServiceImpl(new UserInfoRequestHandlerImpl(introspectionManager,
+                    userInfoResponseBuilder));
+        } catch (UserInfoException e) {
+            throw new IllegalStateException("Could not create UserInfoService", e);
+        }
     }
 
 }
