52be11a827470300fdc8b6426ad60dc83a2ffa98
Rukshan <ruky68@live.com>
2018-04-23 17:29:00 +0530

    Added initial scope validator

---
+++ components/auth/org.wso2.carbon.auth.oauth/src/main/java/org/wso2/carbon/auth/oauth/ScopeValidateHandler.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.carbon.auth.oauth;
+
+import com.nimbusds.oauth2.sdk.ErrorObject;
+import com.nimbusds.oauth2.sdk.OAuth2Error;
+import org.wso2.carbon.auth.oauth.callback.ScopeValidatorCallback;
+import org.wso2.carbon.auth.oauth.exception.OAuthScopeException;
+import org.wso2.carbon.auth.oauth.internal.ServiceReferenceHolder;
+
+/**
+ * Validate the requested scopes
+ */
+public class ScopeValidateHandler {
+
+    /**
+     * Validate the requested scopes
+     *
+     * @param callback ScopeValidatorCallback
+     */
+    public static void validate(ScopeValidatorCallback callback) {
+        ScopeValidator scopeValidator = ServiceReferenceHolder.getInstance().getScopeValidator();
+        try {
+            scopeValidator.process(callback);
+            callback.setSuccessful(true);
+        } catch (OAuthScopeException e) {
+            callback.setSuccessful(false);
+            ErrorObject error = new ErrorObject(OAuth2Error.SERVER_ERROR.getCode());
+            callback.setErrorObject(error);
+        }
+    }
+}
